d9a5fb8d7d27014487936c69ad84ecaea78306fe
KSM1998 <tjdals5655@gmail.com>
2021-01-09 13:45:04 +0000

    list try 5

--- assets/js/list-select.js
+++ assets/js/list-select.js
@@ -1,19 +1,20 @@
+
 var buttons = document.querySelectorAll("li");
 var section = document.querySelectorAll(".each-gallery");
-buttons.forEach(function (item) {
-  item.addEventListener('click', function () {
-    buttons.forEach(function (item) {
-      item.clssName = "";
-    });
-    item.className = "active"; //show images
 
-    var values = item.textContent;
-    section.forEach(function (show) {
-      show.style.display = "none";
+       	 Array.prototype.forEach.call(buttons, function(item){
+          	item.addEventListener('click', function () {
+            Array.prototype.forEach.call(buttons, function(item){
+              item.clssName = "";
+            });
+            item.className = "active"; //show images
 
-      if (show.getAttribute("data-id") === values || values ==="전체") {
-        show.style.display = "block";
-      }
-    });
-  });
-});
+            var values = item.textContent;
+            Array.prototype.forEach.call(section, function(show){
+              show.style.display = "none";
+              if (show.getAttribute("data-id") === values || values === "전체")  {
+               show.style.display = "block";
+              }
+            });            
+           });
+         });
